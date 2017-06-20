b4da6b81be3866e2e10a5f9e82292deaa55949c2
mahahahajan <pulkit8.mahajan@gmail.com>
2017-06-20 11:30:07 -0700

    first build - still needs the UI tho

---
+++ main.js
@@ -0,0 +1,43 @@
+ window.onload = function main() {
+     var form = document.getElementById('scan-form');
+     form.style.display = 'none';
+     var input = document.getElementById('hkb-search');
+     
+     var autocomplete = new google.maps.places.Autocomplete(input, "establishment");
+
+
+     var infowindow = new google.maps.InfoWindow();
+     var infowindowContent = document.getElementById('infowindow-content');
+     infowindow.setContent(infowindowContent);
+     google.maps.event.addListener(autocomplete, 'place_changed', function() {
+         //wanna show form and then populate it 
+         form.style.display = "block";
+         var place = autocomplete.getPlace();
+         //name field
+         var businessName = document.getElementById('businessName');
+         businessName.value = place.name;
+         
+         //phone field
+         var businessPhone = document.getElementById('businessPhone');
+         businessPhone.value = place.formatted_phone_number;
+         
+
+         console.log(place);
+         for(i = 0; i < place.address_components.length; i++){
+         	if(place.address_components[i].types[0] == "postal_code"){
+         		var businessZip = document.getElementById("zip");
+         		console.log(place.address_components[i].short_name);
+         		businessZip.value = place.address_components[i].long_name;	
+         	}
+         }
+         console.log(autocomplete.getPlace().address_components);
+         
+     });
+
+     function fillInAddress() {
+         // Get the place details from the autocomplete object.
+         var autocomplete = autocomplete.getPlace();
+
+     }
+
+ }
