be04901d405c406c137f2232127a4ce8298a80a7
mahahahajan <pulkit8.mahajan@gmail.com>
2017-06-26 14:20:43 -0700

    trying to fix on other browsers

---
+++ apiStyler.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+ 
+ var results = document.getElementsByClassName("pac-item-query");
+ var lineBreaker = document.createElement("br");
+
+ $(results).after("<br />");
+
+});
--- main.js
+++ main.js
@@ -1,50 +1,47 @@
  window.onload = function main() {
-     
+
      //window.location.href = "scanned.html";
+     var types = [ "(establishment)"
+ ];
+ var form = document.getElementById('scan-form');
+ form.style.display = 'none';
+ var input = document.getElementById('hkb-search');
+ var autocomplete = new google.maps.places.Autocomplete(input, {type:["establishment"]});
 
-     var form = document.getElementById('scan-form');
-     form.style.display = 'none';
-     var input = document.getElementById('hkb-search');
-     var autocomplete = new google.maps.places.Autocomplete(input, "establishment");
+
+ var infowindow = new google.maps.InfoWindow();
+ var infowindowContent = document.getElementById('infowindow-content');
+ infowindow.setContent(infowindowContent);
+ var results = document.getElementsByClassName("pac-item");
+ var lineBreaker = document.createElement("br");
+ 
+
+ console.log(results);
+ google.maps.event.addListener(autocomplete, 'place_changed', function() {
+     //wanna show form and then populate it 
+     form.style.display = "block";
+     var seenOn = document.getElementById('seen');
+     // seenOn.style.marginTop = "5%";
+     var place = autocomplete.getPlace();
+     //name field
+     var businessName = document.getElementById('businessName');
+     businessName.value = place.name;
+
+     //phone field
+     var businessPhone = document.getElementById('businessPhone');
+     businessPhone.value = place.formatted_phone_number;
 
 
-     var infowindow = new google.maps.InfoWindow();
-     var infowindowContent = document.getElementById('infowindow-content');
-     infowindow.setContent(infowindowContent);
-     var results = document.getElementsByClassName("pac-container");
-     
-     console.log(results);
-     google.maps.event.addListener(autocomplete, 'place_changed', function() {
-         //wanna show form and then populate it 
-         form.style.display = "block";
-         var seenOn = document.getElementById('seen');
-        // seenOn.style.marginTop = "5%";
-         var place = autocomplete.getPlace();
-         //name field
-         var businessName = document.getElementById('businessName');
-         businessName.value = place.name;
-         
-         //phone field
-         var businessPhone = document.getElementById('businessPhone');
-         businessPhone.value = place.formatted_phone_number;
-         
-
-         console.log(place);
-         for(i = 0; i < place.address_components.length; i++){
-         	if(place.address_components[i].types[0] == "postal_code"){
-         		var businessZip = document.getElementById("zip");
-         		console.log(place.address_components[i].short_name);
-         		businessZip.value = place.address_components[i].long_name;	
-         	}
+     console.log(place);
+     for (i = 0; i < place.address_components.length; i++) {
+         if (place.address_components[i].types[0] == "postal_code") {
+             var businessZip = document.getElementById("zip");
+             console.log(place.address_components[i].short_name);
+             businessZip.value = place.address_components[i].long_name;
          }
-         console.log(autocomplete.getPlace().address_components);
-         
-     });
-
-     function fillInAddress() {
-         // Get the place details from the autocomplete object.
-         var autocomplete = autocomplete.getPlace();
-
      }
+     console.log(autocomplete.getPlace().address_components);
+
+ });
 
  }
